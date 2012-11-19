41caac1407a89bf86bcc1fd4e8b985344e04c2c8
Matt Bornski <matt@bornski.com>
2012-11-19 10:03:11 -0800

    logging will be useful

--- oozie/__init__.py
+++ oozie/__init__.py
@@ -2,6 +2,8 @@
 
 import json
 import urllib2
+import logging
+import os
 import sys
 
 class ClientError(Exception):
@@ -11,12 +13,17 @@ class ServerError(Exception):
     pass
 
 class client(object):
-    def __init__(self, url):
+    def __init__(self, url=None):
+        if url is None:
+            url = os.environ.get('OOZIE_URL')
+        if url is None:
+            raise ClientError('No Oozie URL provided and none set in environment OOZIE_URL')
         self._url = url
     def healthcheck(self):
         try:
             systemMode = json.loads(urllib2.urlopen(self._url + 'v1/admin/status').read().strip())['systemMode']
             assert(systemMode == 'NORMAL')
+            logging.info('Oozie installation at ' + self._url + ' appears operational')
             return True
         except AssertionError:
             raise ServerError(systemMode)
@@ -24,11 +31,14 @@ class client(object):
             raise ClientError(e.message)
         except urllib2.HTTPError as e:
             raise ClientError('HTTP Error ' + str(e.getcode()) + ': ' + e.msg + ' ' + e.geturl())
+    def run(self, workflow):
+        pass
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        c = client(sys.argv[1])
-        c.healthcheck()
-        print 'Oozie installation at ' + sys.argv[1] + ' appears operational'
-    else:
-        print 'Usage: python ' + __file__ + ' <OOZIE_URL>'
+    logging.basicConfig(level=logging.DEBUG)
+    c = client()
+    examples = {
+        'echo': lambda c: c.run({}),
+    }
+    # Run given example name please
+    examples.get((sys.argv + [None])[1], lambda c: c.healthcheck())(c)
